a7d30c493d093889c2a15b009d43cf917ced2edd
Vĩnh Bảo Phúc <ITITIU20127@student.hcmiu.edu.vn>
2023-11-13 19:45:01 +0700

    Update

--- InventoryApp/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ InventoryApp/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.inventory.data
 
 import android.content.Context
@@ -37,11 +21,6 @@ abstract class InventoryDatabase : RoomDatabase() {
             // if the Instance is not null, return it, otherwise create a new database instance.
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
-                    /**
-                     * Setting this option in your app's database builder means that Room
-                     * permanently deletes all data from the tables in your database when it
-                     * attempts to perform a migration with no defined migration path.
-                     */
                     .fallbackToDestructiveMigration()
                     .build()
                     .also { Instance = it }
